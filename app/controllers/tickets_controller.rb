f80f90542c07f6183791aed122e350e744ddfaeb
strand <jet.strand@gmail.com>
2012-01-13 17:13:22 +0800

    Implemented deleting ticket feature

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -27,6 +27,11 @@ class TicketsController < ApplicationController
       render :action => "edit"
     end
   end
+  def destroy
+    @ticket.destroy
+    flash[:notice] = "Ticket has been deleted."
+    redirect_to @project
+  end
   private
     def find_project
       @project = Project.find(params[:project_id])
