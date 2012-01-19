7eccebf30659c2f121dfd01b1e644c17325fcd92
strand <jet.strand@gmail.com>
2012-01-19 14:02:27 +0800

    Lock down specific projects controller actions for admins only

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -15,11 +15,12 @@ describe ProjectsController do
       "edit"    => "get",
       "update"  => "put",
       "destroy" =>  "delete" }.each do |action, method|
-    it "cannot access the #{action} action" do
-      sign_in(:user, user)
-      send(method, action.dup, :id => project.id)
-      response.should redirect_to(root_path)
-      flash[:alert].should eql("You must be an admin to do that.")
+      it "cannot access the #{action} action" do
+        sign_in(:user, user)
+        send(method, action.dup, :id => project.id)
+        response.should redirect_to(root_path)
+        flash[:alert].should eql("You must be an admin to do that.")
+      end
     end
   end
   
