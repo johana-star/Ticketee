6f65cd331752292a2551b2721229432d49bbb857
strand <jet.strand@gmail.com>
2012-02-11 19:59:39 +0800

    Restrict destroy action to only people with permission

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -4,6 +4,7 @@ class TicketsController < ApplicationController
   before_filter :find_ticket, :only => [:show, :edit, :update, :destroy]
   before_filter :authorize_create!, :only => [:new, :create]
   before_filter :authorize_update!, :only => [:edit, :update]
+  before_filter :authorize_delete!, :only => [:destroy]
   def new
     @ticket = @project.tickets.build
   end
@@ -52,8 +53,14 @@ class TicketsController < ApplicationController
       end
     end
     def authorize_update!
-      if !current_user.admin? && cannot?(:"edit tickets".to_sym, @project)
+      if !current_user.admin? && cannot?(:"edit tickets", @project)
         flash[:alert] = "You cannot edit tickets on this project."
+        redirect_to @project
+      end
+    end
+    def authorize_delete!
+      if !current_user.admin? && cannot?(:"delete tickets", @project)
+        flash[:alert] = "You cannot delete tickets from this project."
         redirect_to @project
       end
     end
